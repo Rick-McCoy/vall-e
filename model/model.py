76d1a22078ac4dc1b7eab03f3b03f65ece5f1787
Rick-McCoy <rickmccoy3141@gmail.com>
2023-05-13 00:42:46 +0900

    Model forward implemented

--- model/model.py
+++ model/model.py
@@ -26,11 +26,14 @@ class VallE(LightningModule):
         )
         self.logger: WandbLogger
 
-    def log_table(self, image: Tensor, pred: Tensor, header: str):
+    def log_table(self, real_audio: Tensor, gen_audio: Tensor, header: str):
         self.logger.log_table(
             f"{header}/table",
-            columns=["Image", "Prediction"],
-            data=[[wandb.Image(image.detach().cpu().numpy()), pred.item()]],
+            columns=["Real", "Generated"],
+            data=[
+                wandb.Audio(real_audio, sample_rate=self.cfg.data.sample_rate),
+                wandb.Audio(gen_audio, sample_rate=self.cfg.data.sample_rate),
+            ],
         )
 
     def parse_batch(self, data: CollatedBatch):
@@ -54,7 +57,7 @@ class VallE(LightningModule):
         ar_output_batch = self.autoregressive(
             text, audio, enrolled_audio[:, 0], text_len_batch, audio_len_batch
         )
-        ar_output_list = []
+        ar_output_split = []
         for ar_output, text_len, audio_len in zip(
             ar_output_batch, text_len_batch, audio_len_batch
         ):
@@ -69,15 +72,22 @@ class VallE(LightningModule):
                     + audio_len_item
                     - 1
                 ].argmax(dim=-1),
-                (0, self.cfg.data.max_audio_len - audio_len_item),
+                (0, audio.shape[0] - audio_len_item),
             )
-            ar_output_list.append(ar_output)
-        ar_output_batch = torch.stack(ar_output_list)
+            ar_output_split.append(ar_output)
+        ar_padded_output = torch.stack(ar_output_split, dim=0)
+
+        nar_output_list = []
         for i in range(1, self.cfg.data.codec_channels):
             nar_output_batch = self.nonautoregressive(
-                text, audio, enrolled_audio, text_len_batch, audio_len_batch, i
+                text,
+                audio[:, i - 1],
+                enrolled_audio,
+                text_len_batch,
+                audio_len_batch,
+                i,
             )
-            nar_output_list = []
+            nar_output_split = []
             for nar_output, text_len, audio_len in zip(
                 nar_output_batch, text_len_batch, audio_len_batch
             ):
@@ -92,39 +102,40 @@ class VallE(LightningModule):
                     ].argmax(dim=-1),
                     (0, self.cfg.data.max_audio_len - audio_len_item),
                 )
-                nar_output_list.append(nar_output)
-            nar_output_batch = torch.stack(nar_output_list)
+                nar_output_split.append(nar_output)
+            nar_output_batch = torch.stack(nar_output_split, dim=0)
+            nar_output_list.append(nar_output_batch)
+
+        return torch.cat([ar_padded_output] + nar_output_list, dim=1)
 
     def training_step(self, batch: CollatedBatch, *args, **kwargs) -> Tensor:
         text, text_len, audio, audio_len, enrolled_audio = self.parse_batch(batch)
-        output = self(data)
-        loss = self.loss(output, label)
-        self.acc(output, label)
-        self.log("train/loss", loss, on_step=True, prog_bar=True)
-        self.log("train/acc", self.acc, on_step=True, prog_bar=True)
+        output = self(text, audio, enrolled_audio, text_len, audio_len)
+        loss = self.loss(output, audio)
+        self.acc(output, audio)
+        self.log("train/loss", loss, on_epoch=True, sync_dist=True)
+        self.log("train/acc", self.acc, on_epoch=True, sync_dist=True)
         return loss
 
     def validation_step(self, batch: CollatedBatch, batch_idx: int, *args, **kwargs):
-        data, label = batch
-        output = self(data)
-        loss = self.loss(output, label)
-        self.acc(output, label)
+        text, text_len, audio, audio_len, enrolled_audio = self.parse_batch(batch)
+        output = self(text, audio, enrolled_audio, text_len, audio_len)
+        loss = self.loss(output, audio)
         self.log("val/loss", loss, on_epoch=True, sync_dist=True)
         self.log("val/acc", self.acc, on_epoch=True, sync_dist=True)
         if batch_idx == 0 and self.device.index == 0:
             pred = torch.argmax(output[0], dim=-1)
-            self.log_table(data[0], pred, "val")
+            self.log_table(audio[0], pred, "val")
 
     def test_step(self, batch: CollatedBatch, batch_idx: int, *args, **kwargs):
-        data, label = batch
-        output = self(data)
-        loss = self.loss(output, label)
-        self.acc(output, label)
+        text, text_len, audio, audio_len, enrolled_audio = self.parse_batch(batch)
+        output = self(text, audio, enrolled_audio, text_len, audio_len)
+        loss = self.loss(output, audio)
         self.log("test/loss", loss, on_epoch=True, sync_dist=True)
         self.log("test/acc", self.acc, on_epoch=True, sync_dist=True)
         if batch_idx == 0 and self.device.index == 0:
             pred = torch.argmax(output[0], dim=-1)
-            self.log_table(data[0], pred, "test")
+            self.log_table(audio[0], pred, "val")
 
     def configure_optimizers(self):
         return torch.optim.Adam(
