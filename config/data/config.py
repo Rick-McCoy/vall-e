f15aef34df16e11da5e6765867f036c272ae5c68
Rick-McCoy <rickmccoy3141@gmail.com>
2023-05-18 11:44:46 +0900

    Fix typo

--- config/data/config.py
+++ config/data/config.py
@@ -5,7 +5,7 @@ from pathlib import Path
 @dataclass
 class DataConfig:
     path: Path = Path(".")
-    train_val_split: float = 0.02
+    train_val_split: float = 0.98
     sample_rate: int = 24000
     channels: int = 1
     codec_rate: int = 75
