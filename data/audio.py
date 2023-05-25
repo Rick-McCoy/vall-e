Enabled DDP Fixed Tensorboard Added train.wandb Added gradient checkpointing

data/audio.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import Optional, Tuple, cast
 
 import librosa
 import numpy as np
@@ -8,6 +8,7 @@ import torch
 
 from encodec.model import EncodecModel, EncodedFrame
 from encodec.modules.lstm import SLSTM
+from utils.model import remove_weight_norm
 
 
 def load_audio(path: Path, target_sr: int, channels: int) -> np.ndarray:
@@ -81,16 +82,39 @@ def audio_to_codec(audio: torch.Tensor, encodec_model: EncodecModel) -> torch.Te
         return torch.cat([frame[0] for frame in frames], dim=-1)
 
 
-def codec_to_audio(codec: torch.Tensor, encodec_model: EncodecModel) -> torch.Tensor:
+def codec_to_audio(
+    codec: torch.Tensor,
+    encodec_model: Optional[EncodecModel] = None,
+    sample_rate: Optional[int] = None,
+) -> torch.Tensor:
     """Decodes a codec tensor to audio.
 
     Args:
         codec (torch.Tensor): Codec tensor. Shape: (batch, 8, codec_samples)
-        encodec_model (EncodecModel): Encodec model to use for decoding.
+        encodec_model (Optional[EncodecModel]): Encodec model to use for decoding.
+        If None, uses the sample_rate argument to create a new model.
+        sample_rate (Optional[int]): Sample rate to use for creating a new model.
 
     Returns:
         audio (torch.Tensor): Audio tensor. Shape: (batch, channels, codec_samples * compression_factor)
     """
+    if encodec_model is None:
+        assert sample_rate is not None
+        if sample_rate == 24000:
+            encodec_model = EncodecModel.encodec_model_24khz()
+        elif sample_rate == 48000:
+            encodec_model = EncodecModel.encodec_model_48khz()
+        else:
+            raise NotImplementedError(f"Sample rate {sample_rate} not supported")
+        remove_weight_norm(encodec_model)
+        encodec_model = cast(
+            EncodecModel,
+            torch.jit.script(  # pyright: ignore [reportPrivateImportUsage]
+                encodec_model
+            ),
+        )
+        encodec_model = encodec_model.to(codec.device)
+
     with torch.no_grad():
         if encodec_model.segment is None:
             frames: list[EncodedFrame] = [(codec, None)]

main.py
@@ -22,7 +22,8 @@ from config.model.config import ModelConfig
 from config.train.config import TrainConfig
 from data.datamodule import VallEDataModule
 from model.model import VallE
-from utils.model import remove_weight_norm
+
+# from utils.model import remove_weight_norm
 
 cs = ConfigStore.instance()
 cs.store(name="config", node=Config)
@@ -34,19 +35,16 @@ cs.store(group="model", name="base_model", node=ModelConfig)
 @hydra.main(config_path="config", config_name="config", version_base=None)
 def main(cfg: Config):
     model = VallE(cfg)
-    compiled_model = torch.compile(model, disable=True)
-    compiled_model = cast(VallE, compiled_model)
+    compiled_model = cast(VallE, torch.compile(model, disable=True))
     datamodule = VallEDataModule(cfg)
 
     Path("logs").mkdir(exist_ok=True)
     if cfg.train.fast_dev_run:
-        logger = TensorBoardLogger(
-            save_dir="logs",
-            name="fast_dev_run",
-            log_graph=True,
-        )
-    else:
+        logger = None
+    elif cfg.train.wandb:
         logger = WandbLogger(project=cfg.train.project, save_dir="logs")
+    else:
+        logger = TensorBoardLogger(save_dir="logs", name="vall-e")
 
     callbacks = []
 
@@ -88,14 +86,12 @@ def main(cfg: Config):
     precision = cfg.train.precision
     assert precision == "32" or precision == "16-mixed"
     trainer = Trainer(
-        accelerator="auto",
-        strategy="ddp_find_unused_parameters_true",
+        strategy="ddp",
         accumulate_grad_batches=cfg.train.acc,
         callbacks=callbacks,
         detect_anomaly=True,
-        devices="auto",
         fast_dev_run=cfg.train.fast_dev_run,
-        logger=[logger],
+        logger=logger,
         log_every_n_steps=10,
         max_steps=cfg.train.max_steps,
         num_sanity_val_steps=10,
@@ -112,15 +108,15 @@ def main(cfg: Config):
     trainer.fit(model=compiled_model, datamodule=datamodule)
     trainer.test(model=compiled_model, datamodule=datamodule)
 
-    remove_weight_norm(compiled_model)
-    script_model = torch.jit.script(  # pyright: ignore [reportPrivateImportUsage]
-        compiled_model
-    )
-    save_path = Path("model-store")
-    save_path.mkdir(exist_ok=True)
-    torch.jit.save(  # pyright: ignore [reportPrivateImportUsage]
-        script_model, save_path / "model.pt"
-    )
+    # remove_weight_norm(compiled_model)
+    # script_model = torch.jit.script(  # pyright: ignore [reportPrivateImportUsage]
+    #     compiled_model
+    # )
+    # save_path = Path("model-store")
+    # save_path.mkdir(exist_ok=True)
+    # torch.jit.save(  # pyright: ignore [reportPrivateImportUsage]
+    #     script_model, save_path / "model.pt"
+    # )
 
 
 if __name__ == "__main__":

model/model.py
@@ -1,4 +1,4 @@
-from typing import Literal, cast
+from typing import Literal
 
 import numpy as np
 import torch
@@ -12,12 +12,10 @@ from config.config import Config
 from data.audio import codec_to_audio, mel_energy
 from data.datamodule import CollatedBatch
 from data.text import CHAR_TO_CODE, VOCAB_SIZE, encode_text
-from encodec.model import EncodecModel
 from model.autoregressive import AutoRegressive
 from model.loss import VallELoss
 from model.nonautoregressive import NonAutoRegressive
 from utils.data import plot_mel_spectrogram
-from utils.model import remove_weight_norm
 from utils.utils import unpad_sequence
 
 
@@ -29,6 +27,7 @@ class VallE(LightningModule):
             cfg.data.enrolled_audio_length * cfg.data.codec_rate
         )
         self.codec_channels = cfg.data.codec_channels
+        self.sample_rate = cfg.data.sample_rate
         self.register_buffer(
             "text_eos", torch.tensor([CHAR_TO_CODE["<EOS>"]], dtype=torch.long)
         )
@@ -60,21 +59,6 @@ class VallE(LightningModule):
             torch.tensor([50]),
             torch.tensor([30]),
             torch.tensor([200]),
-        )
-        if cfg.data.sample_rate == 24000:
-            encodec_model = EncodecModel.encodec_model_24khz()
-        elif cfg.data.sample_rate == 48000:
-            encodec_model = EncodecModel.encodec_model_48khz()
-        else:
-            raise NotImplementedError(
-                f"Sample rate {cfg.data.sample_rate} not supported"
-            )
-        remove_weight_norm(encodec_model)
-        self.encodec_model = cast(
-            EncodecModel,
-            torch.jit.script(  # pyright: ignore [reportPrivateImportUsage]
-                encodec_model
-            ),
         )
         self.register_buffer(
             "gen_text",
@@ -411,7 +395,7 @@ class VallE(LightningModule):
             codec_list = [longest_audio, pred, gen]
         data = []
         for codec in codec_list:
-            audio = codec_to_audio(codec, self.encodec_model)
+            audio = codec_to_audio(codec, sample_rate=self.sample_rate)
             mel, _ = mel_energy(
                 audio.squeeze(1),
                 n_fft=1024,
@@ -434,21 +418,22 @@ class VallE(LightningModule):
                 data=[
                     [
                         wandb.Audio(audio, sample_rate=self.cfg.data.sample_rate),
-                        wandb.Image(mel),
+                        wandb.Image(mel_image, mode="RGBA"),
                     ]
-                    for (audio, mel) in data
+                    for (audio, mel_image) in data
                 ],
             )
         elif isinstance(self.logger, TensorBoardLogger):
-            for (mel, audio), name in zip(data, ["real", "pred", "gen"]):
+            for (audio, mel_image), name in zip(data, ["real", "pred", "gen"]):
                 self.logger.experiment.add_audio(
                     f"{mode}/Audio/{name}",
-                    audio,
+                    audio / 2**15,
                     self.global_step,
                     sample_rate=self.cfg.data.sample_rate,
                 )
                 self.logger.experiment.add_image(
                     f"{mode}/Mel/{name}",
-                    mel,
+                    mel_image,
                     self.global_step,
+                    dataformats="HWC",
                 )

model/transformer.py
@@ -1,5 +1,6 @@
 from typing import Callable, Optional
 
+import torch.utils.checkpoint
 from torch import Tensor, nn
 from torch.nn import functional as F
 from torch.nn.modules.activation import MultiheadAttention
@@ -21,19 +22,21 @@ class TransformerDecoder(nn.TransformerDecoder):
         output = tgt
 
         for mod in self.layers:
-            output = mod(
+            output = torch.utils.checkpoint.checkpoint(
+                mod,
                 output,
                 memory,
                 layer,
-                tgt_mask=tgt_mask,
-                memory_mask=memory_mask,
-                tgt_key_padding_mask=tgt_key_padding_mask,
-                memory_key_padding_mask=memory_key_padding_mask,
+                tgt_mask,
+                memory_mask,
+                tgt_key_padding_mask,
+                memory_key_padding_mask,
             )
 
         if self.norm is not None:
             output = self.norm(output)
 
+        assert output is not None
         return output
 
 